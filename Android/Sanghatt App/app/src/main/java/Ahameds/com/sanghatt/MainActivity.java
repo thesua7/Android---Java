947f6e3c5c766796334c4f77ea1a854d37595741
Sani Ahamed <thesua711@gmail.com>
2019-11-21 19:09:32 +0600

    pull

--- Android/Sanghatt App/app/src/main/java/Ahameds/com/sanghatt/MainActivity.java
+++ Android/Sanghatt App/app/src/main/java/Ahameds/com/sanghatt/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity {
                     Show.setText("Added");
                 }
                 else{
-                    Show.setText(FName.getText());
+                    Show.setText(FName.getText());//ok
                 }
             }
         });
