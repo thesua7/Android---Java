82b0eaea0a9d70afc74dce8049cb7f3b6d17d571
Sani Ahamed <thesua711@gmail.com>
2019-11-21 18:58:31 +0600

    db added (insertion)

--- Android/Sanghatt App/app/src/main/java/Ahameds/com/sanghatt/MainActivity.java
+++ Android/Sanghatt App/app/src/main/java/Ahameds/com/sanghatt/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageButton india;
     Dialog popup;
+    DatabaseHelper myDB;
+
 
 
 
@@ -20,8 +24,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        popup = new Dialog(this);
-
+        popup = new Dialog(this);//POPUP
+        myDB = new DatabaseHelper(this);
+        //myDB.getWritableDatabase();
         india = (ImageButton)findViewById(R.id.indiajar);
 
         india.setOnClickListener(new View.OnClickListener() {
@@ -33,13 +38,24 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+
     }
 
 
        public void Showpopup(View v){
            TextView exit;
+           final TextView Show;
+           final EditText FName,Email,Phone;
+           Button AddUsers;
+
         popup.setContentView(R.layout.custompopup);
         exit = (TextView) popup.findViewById(R.id.exitbtn);
+        FName = (EditText) popup.findViewById(R.id.fullname);
+        Email = (EditText) popup.findViewById(R.id.emailid);
+        Phone = (EditText) popup.findViewById(R.id.phonenumber);
+        Show = (TextView) popup.findViewById(R.id.test);
+
+        AddUsers = (Button) popup.findViewById(R.id.addbtn);
 
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +65,18 @@ public class MainActivity extends AppCompatActivity {
         });
 
         popup.show();
+        AddUsers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Boolean flag =myDB.insertData(FName.getText().toString(),Email.getText().toString(),Phone.getText().toString(),"India");
+                if(flag==true){
+                    Show.setText("Added");
+                }
+                else{
+                    Show.setText(FName.getText());
+                }
+            }
+        });
 
        }
 
